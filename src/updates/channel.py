0381625815245a21739d86dc105ff56d237f7c96
Yevgeniy Kap <kapustaevg@gmail.com>
2024-04-29 12:55:10 +0300

    add log for handle_new_subscriber

--- src/updates/channel.py
+++ src/updates/channel.py
@@ -1,6 +1,7 @@
 from aiogram import Router
 from aiogram.enums import ChatMemberStatus
 from aiogram.types import ChatMemberUpdated
+from loguru import logger
 
 from src.services.channel_statistic import update_new_subscribers
 
@@ -9,5 +10,6 @@ router = Router()
 
 @router.chat_member()
 async def handle_new_subscriber(event: ChatMemberUpdated):
+    logger.debug(event.new_chat_member.status)
     if event.new_chat_member.status == ChatMemberStatus.CREATOR:
         await update_new_subscribers(event.date.date(), event.chat.id)
